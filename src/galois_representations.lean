825d22ec3b84ddfbcc656d653c1536dfdcd61a3d
Michail Karatarakis <40603357+mkaratarakis@users.noreply.github.com>
2023-02-20 00:35:01 +0100

    Update galois_representations.lean

--- src/galois_representations.lean
+++ src/galois_representations.lean
@@ -1,12 +1,15 @@
 import field_theory.is_alg_closed.algebraic_closure
-import linear_algebra.general_linear_group
 import topology.algebra.continuous_monoid_hom
 import field_theory.krull_topology
-
+import linear_algebra.matrix.general_linear_group
+import frobenius
+import topology.instances.matrix
 
 variables (k : Type*) [comm_ring k] [topological_space k] [topological_ring k]
 
 /- The type of Galois representations -/
 
-noncomputable! def galois_rep (p : ℕ) (h: prime p) := continuous_monoid_hom
- ((algebraic_closure ℚ) ≃ₐ[ℚ] (algebraic_closure ℚ) ) (GL (fin p) k)
+
+noncomputable! def galois_rep := continuous_monoid_hom
+ ((algebraic_closure ℚ) ≃ₐ[ℚ] (algebraic_closure ℚ) ) (GL (fin 2) k)
+
